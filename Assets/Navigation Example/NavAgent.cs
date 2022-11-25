69bcd31b3851875ae4c333150d0517e1be0f0f02
Kirk Friend <kirkkaf13@gmail.com>
2022-11-25 09:22:00 +0000

    Added agent ability to move along waypoints

---
+++ Assets/Navigation Example/NavAgent.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavAgent : MonoBehaviour
+{
+
+    public AIWaypointNetwork WaypointNetwork = null;
+    public int CurrentWaypoint = 0;
+
+    private NavMeshAgent _agent = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+
+        if (WaypointNetwork == null) return;
+
+        SetNextDestination(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!_agent.hasPath && !_agent.pathPending)
+            SetNextDestination(true);
+
+    }
+
+    void SetNextDestination(bool increment)
+    {
+        int incrementStep = increment ? 1 : 0;
+        Transform nextWaypointTramsform = null;
+
+        while (nextWaypointTramsform == null)
+        {
+
+            int nextDestinationIndex = (CurrentWaypoint + incrementStep >= WaypointNetwork.Waypoints.Count) ? 0 : CurrentWaypoint + incrementStep;
+            nextWaypointTramsform = WaypointNetwork.Waypoints[nextDestinationIndex];
+
+            if (nextWaypointTramsform != null)
+            {
+                CurrentWaypoint = nextDestinationIndex;
+                _agent.destination = nextWaypointTramsform.position;
+            }
+        }
+    }
+}
