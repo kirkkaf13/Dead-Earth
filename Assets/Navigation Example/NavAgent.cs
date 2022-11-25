04a3b7e1b56a3670a782d55a15a81c5c0267fd0b
Kirk Friend <kirkkaf13@gmail.com>
2022-11-25 17:39:31 +0000

    Handle off mesh links and jumping

--- Assets/Navigation Example/NavAgent.cs
+++ Assets/Navigation Example/NavAgent.cs
@@ -9,6 +9,7 @@ public class NavAgent : MonoBehaviour
 
     public AIWaypointNetwork WaypointNetwork = null;
     public int CurrentWaypoint = 0;
+    public AnimationCurve animationCurve = null;
 
     private NavMeshAgent _agent = null;
     // Start is called before the first frame update
@@ -26,11 +27,34 @@ public class NavAgent : MonoBehaviour
     void Update()
     {
 
+        if (_agent.isOnOffMeshLink)
+        {
+            StartCoroutine(Jump(1.0f));
+            return;
+        }
+
         if (!_agent.hasPath && !_agent.pathPending || _agent.pathStatus != NavMeshPathStatus.PathComplete)
             SetNextDestination(true);
         else if (_agent.isPathStale)
             SetNextDestination(false);
 
+    }
+
+    IEnumerator Jump(float duration)
+    {
+        var data = _agent.currentOffMeshLinkData;
+        var startPos = _agent.transform.position;
+        var endPos = data.endPos + (_agent.baseOffset * Vector3.up);
+        var time = 0.0f;
+
+        while (time <= duration)
+        {
+            var t = time / duration;
+            _agent.transform.position = Vector3.Lerp(startPos, endPos, t) + animationCurve.Evaluate(t) * Vector3.up;
+            time += Time.deltaTime;
+            yield return null;
+        }
+        _agent.CompleteOffMeshLink();
     }
 
     void SetNextDestination(bool increment)
